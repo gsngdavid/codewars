68b58805ff533322cccc8dd5bf38efd849de8331
gsngdavid <gsngdavid@gmail.com>
2023-07-27 12:33:22 +0200

    feat: validate braces imporvement

--- 6kyu/Valid Braces.js
+++ 6kyu/Valid Braces.js
@@ -1,31 +1,12 @@
-function validBraces(braces) {
-    if(braces.length === 0) return true;
-    const closingChar = getClosingChar(braces[0]);
-    if(!closingChar) return false;
-    let closingCharIndex = braces.indexOf(closingChar);
-    if(closingCharIndex === -1) return false;
-
-    let subString = braces.slice(1, closingCharIndex);
-    if(subString.includes(braces[0])) closingCharIndex = braces.lastIndexOf(closingChar);
+function validBraces(braces){
+    const matches = {'(': ')', '{': '}', '[': ']'};
+    const stack = [];
     
-    const subStringIsValid = validateSubString(braces.slice(1, closingCharIndex));
+    for(let i = 0; i < braces.length; i++) {
+      const cur = braces[i];
+      if(matches[cur]) stack.push(cur);
+      else if(matches[stack.pop()] !== cur) return false;
+    }
     
-    if(!subStringIsValid) return false;
-    return validBraces(braces.slice(closingCharIndex + 1));
-}
-
-function validateSubString(subStr) {
-    if(subStr.length === 0) return true;
-    const closingChar = getClosingChar(subStr[0]);
-    if(!closingChar) return false;
-    const closingCharIndex = subStr.lastIndexOf(closingChar);
-    if(closingCharIndex === subStr.length - 1) return validateSubString(subStr.slice(1, subStr.length - 1));
-    return false;
-}
-
-function getClosingChar(char) {
-    if(char === '(') return ')';
-    if(char === '{') return '}';
-    if(char === '[') return ']';
-    return false;
+    return !stack.length;
 }
