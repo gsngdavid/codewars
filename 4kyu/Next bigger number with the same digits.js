d385fdbe20dfb10bd8608ae9563580625c3018d4
gsngdavid <gsngdavid@gmail.com>
2023-08-05 21:39:22 +0200

    feat: Refactor Next bigger number with the same digits

--- 4kyu/Next bigger number with the same digits.js
+++ 4kyu/Next bigger number with the same digits.js
@@ -1,28 +1,19 @@
-function getLargest(str) {
-    return str.split('').sort((a,b) => b - a).join('');
-}
-
 function nextBigger(n){
-    let strNum = ""+n;
-    let largest = getLargest(strNum);
-    if( largest <= strNum) return -1;
+    let strNum = ""+n;    
+    let result = '';
+    let i = strNum.length - 1;
     
-    let result = null;
-    let i = strNum.length - 2;
-    
-    while(i >= 0) {
-        let end = strNum.slice(i);
-        let largestEnd = getLargest(end);
-
-        if( largestEnd <= end) {
+    while(i > 0) {
+        if(strNum[i] <= strNum[i - 1]) {
             i--;
             continue;
         }
-        const sortedEndArr = end.split('').sort();
-        const nextChar = sortedEndArr.find(e => e > strNum[i]);
+        const sortedEndArr = strNum.slice(i - 1).split('').sort();
+        const nextChar = sortedEndArr.find(e => e > strNum[i - 1]);
         const nextCharIndex = sortedEndArr.indexOf(nextChar);
-        result = strNum.slice(0, i) + sortedEndArr.splice(nextCharIndex, 1)[0] + sortedEndArr.join('');
+        result = strNum.slice(0, i - 1) + sortedEndArr.splice(nextCharIndex, 1)[0] + sortedEndArr.join('');
 
         return +result;
     }
+    return -1;
 }
