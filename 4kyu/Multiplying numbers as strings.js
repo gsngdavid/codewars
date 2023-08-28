95ab1de0df239d01c279d5016ed692978cd5a2a4
gsngdavid <gsngdavid@gmail.com>
2023-08-28 11:58:05 +0200

    feat: Refactor

--- 4kyu/Multiplying numbers as strings.js
+++ 4kyu/Multiplying numbers as strings.js
@@ -1,52 +1,5 @@
 function multiply(a, b) {
-    let carry = 0;
-    const products = [];
-
-    for(let i = b.length - 1, zeros = 0; i >= 0; i--, zeros++) {
-        let product = '';
-        for(let j = a.length - 1; j >= 0; j--) {
-            let result = String((+b[i] * +a[j]) + carry);
-
-            if(j === 0) {
-                product = result + product;
-                carry = 0;
-            }
-            else {
-                product = (result[1] ?? result[0]) + product;
-                carry = Number(result[1] ? result[0] : 0);
-            }
-        }
-        products.push(product + '0'.repeat(zeros));
-    }
+    const BigInt = 1n.__proto__.constructor;
     
-    return addingArrayElements(products);
-}
-
-function addingArrayElements(arr) {
-    let carry = 0;
-    let finalResult = '';
-
-    const colNumber = arr[arr.length - 1].length;
-    const transformedArr = arr.map(el => {
-        if(el.length < colNumber) {
-            el = "0".repeat(colNumber - el.length) + el;
-        }
-        return el;
-    });
-
-    for(let col = colNumber - 1; col >= 0; col--) {
-        let result = carry;
-        for(let row = 0; row < transformedArr.length; row ++) {
-            result += +transformedArr[row][col];
-        }
-        if(col === 0) {
-            finalResult = String(result) + finalResult;
-            carry = 0;
-            continue;
-        }
-        finalResult = String(result)[1] ? String(result).slice(-1) + finalResult : String(result)[0] + finalResult;
-        carry = String(result)[1] ? +String(result).slice(0, -1) : 0;
-    }
-    finalResult = finalResult.replace(/^0+/, '');
-    return finalResult ? finalResult : '0';
-}
+    return (BigInt(a) * BigInt(b)).toString();
+  }
