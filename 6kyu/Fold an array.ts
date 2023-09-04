981db330c6ceb577c2db6af6e7a05abd8e808415
gsngdavid <gsngdavid@gmail.com>
2023-09-04 12:07:04 +0200

    Refactor

--- 6kyu/Fold an array.ts
+++ 6kyu/Fold an array.ts
@@ -1,21 +1,11 @@
 export function foldArray(array:number[], runs:number):number[] {
-    let myArray: number[] = [];
-    let result: number[] = [];
-    myArray = [...myArray, ...array];
-    array = [];
+    const result = [...array];
     
-    while(runs !== 0) {
-        let stop = Math.floor(myArray.length / 2);
-        for(let i = 0; i < stop; i++) {
-            result.push(myArray.shift()! + myArray.pop()!);
-        }
-        myArray[0] && result.push(myArray.shift()!);
+    while(runs > 0) {
+        result.forEach((val, i, arr) => {
+            arr[i] = i + 1 === arr.length ? val : val + arr.pop()!;
+        });
         runs--;
-        if(runs !== 0) {
-            myArray = result;
-            result = [];
-        } 
     }
-    if(result.length === 0) return [0];
     return result;
 }
