b77c03040e99360eca31538c27140e8de6d32997
gsngdavid <gsngdavid@gmail.com>
2023-09-19 11:48:57 +0200

    Refactor

--- 5kyu/Josephus Permutation.ts
+++ 5kyu/Josephus Permutation.ts
@@ -1,15 +1,10 @@
 export const josephus = <T>(items: T[], k: number): T[] => {
     let result: T[] = [];
-    let i = 0;
-    let count = 1;
-
-    while(items.length > 0) {
-        if(count === k) {
-            result.push(items.splice(i--, 1)[0]);
-            count = 0;
-        }
-        if(++i === items.length) i = 0;
-        count++;
+    let i = (k - 1) % items.length;
+    
+    while(items.length) {
+        result.push(items.splice(i, 1)[0]);
+        i = (i + k - 1) % items.length;
     }
     return result;
 }
