c7e96d855cbd17501cac7cb72a8a8aaeecf603eb
gsngdavid <gsngdavid@gmail.com>
2023-09-20 11:35:46 +0200

    Solve Make the Deadfish Swim

---
+++ 6kyu/Make the Deadfish Swim.ts
@@ -0,0 +1,25 @@
+/** return the output array and ignore all non-op characters */
+export function parse(data: string): number[] {
+    const result: number[] = [];
+    let value = 0;
+
+    for(let char of data) {
+        switch(char) {
+            case 'i':
+                value++;
+                break;
+            case 'd':
+                value--;
+                break;
+            case 's':
+                value *= value;
+                break;
+            case 'o':
+                result.push(value);
+                break;
+            default:
+                continue;
+        }
+    }
+    return result;
+  }
