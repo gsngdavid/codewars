21f85c036605b88a77a565d1beeac276f41345d1
gsngdavid <gsngdavid@gmail.com>
2023-10-03 11:56:55 +0200

    Complete Turkish Numbers, 0-99

---
+++ 7kyu/Turkish Numbers, 0-99.js
@@ -0,0 +1,10 @@
+const getTurkishNumber = (num) => {
+	const names = ['sıfır', 'bir', 'iki', 'üç', 'dört', 'beş', 'altı', 'yedi', 'sekiz', 'dokuz', 'on', 'yirmi', 'otuz', 'kırk', 'elli', 'altmış', 'yetmiş', 'seksen', 'doksan']
+    num += '';
+
+    if(num[1] === '0') return names[+num[0] + 9];
+    if(num[1]) return `${names[+num[0] + 9]} ${names[+num[1]]}`;
+    return names[+num];
+}
+
+console.log(getTurkishNumber(94));
